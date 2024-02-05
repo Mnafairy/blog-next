8439aaf5f534b28465b80024c88935ffc5e5c46d
Оргилсайхан Чулуунболор <23LP8480@imac888.local>
2024-02-05 13:17:59 -0800

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,37 @@
 import { Blogs, Trending, Highlight } from "@/components";
+import { useState, useEffect } from "react";
 const Home = ({ blogs, highlights, trendings }) => {
+  const [article, setArticle] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const [highlight, setHighglight] = useState([]);
+  useEffect(() => {
+    async function getArticle() {
+      const res = await fetch("http://localhost:4000/api/blogs");
+      const data = await res.json();
+      console.log(data);
+      setArticle(data);
+    }
+    getArticle();
+    async function getTrending() {
+      const res = await fetch("http://localhost:4000/api/trendings");
+      const data = await res.json();
+      setTrending(data);
+    }
+    getTrending();
+    async function getHighlight() {
+      const res = await fetch("http://localhost:4000/api/trendings");
+      const data = await res.json();
+      setHighglight(data);
+    }
+    getHighlight();
+  }, []);
   return (
-    <div className="flex flex-col gap-[100px]">
-      <Highlight highlights={highlights} />
-      <Trending trendings={trendings} />
-      <Blogs blogs={blogs} />
-    </div>
+    <main className="flex flex-col gap-[100px]">
+      <p>{article?.title}</p>
+      <Highlight highlights={highlight} />
+      <Trending trendings={trending} />
+      <Blogs blogs={article} />
+    </main>
   );
 };
 export default Home;
@@ -21,4 +47,3 @@ export const getStaticProps = async () => {
   const blogs = await blog.json();
   return { props: { blogs, highlights, trendings } };
 };
-  
