60934e7ef070be470963a8e2099acb95d05f5ee8
Оргилсайхан Чулуунболор <23LP8480@imac888.local>
2024-02-07 10:09:25 -0800

    deleted speedinsights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,13 @@
 import { Work_Sans } from "next/font/google";
 import "@/styles/globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import Layout from "../components/layout";
 const WorkSans = Work_Sans({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
   return (
     <main className={WorkSans.className}>
-      <SpeedInsights>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SpeedInsights>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </main>
   );
 }
