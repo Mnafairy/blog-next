a2cfc0627effa997477eb4101605da0668e3ddda
Оргилсайхан Чулуунболор <23LP8480@imac888.local>
2024-02-07 10:06:44 -0800

    speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import { Work_Sans } from "next/font/google";
 import "@/styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Layout from "../components/layout";
 const WorkSans = Work_Sans({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
   return (
     <main className={WorkSans.className}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SpeedInsights>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SpeedInsights>
     </main>
   );
 }
